c3b290b11eb3ccfbd4bb8502ea3bb62cb29327aa
Sam Hamm <samhamm@gmail.com>
2017-02-09 08:43:18 -0800

    initial commit

---
+++ scripts/article.js
@@ -0,0 +1,39 @@
+var allArticles = [];
+
+function Article (opts) {
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone().removeClass('template');
+
+  $newArticle.attr('data-category', this.category);
+  // TODO: Use jQuery to also add the author name as a data-attribute of the newly cloned article.
+  //       Doing so will allow us to use selectors to target articles, based on who wrote them.
+
+  $newArticle.find('.byline a').text(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('h1:first').text(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  return $newArticle;
+};
+
+sourceData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+sourceData.forEach(function(ele) {
+  allArticles.push(new Article(ele));
+});
+
+allArticles.forEach(function(a){
+  $('#articles').append(a.toHtml());
+});
---
+++ scripts/articleView.js
@@ -0,0 +1,65 @@
+//  REVIEW: Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
+var articleView = {};
+
+articleView.populateFilters = function() {
+  $('article').not('.template').each(function() {
+    var authorName, category, optionTag;
+    authorName = $(this).find('address a').text();
+    optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+    $('#author-filter').append(optionTag);
+    category = $(this).attr('data-category');
+    optionTag = '<option value="' + category + '">' + category + '</option>';
+    if ($('#category-filter option[value="' + category + '"]').length === 0) {
+      $('#category-filter').append(optionTag);
+    }
+  });
+};
+
+articleView.handleAuthorFilter = function() {
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      /* TODO: If the select box changes to an option that has a value, we should:
+          1. Hide all of the articles
+          2. Fade in only the articles that match based on on the author
+            that was aselected. Hint: use an attribute selector to find
+            those articles that match the value, and then fade them in.
+        */
+    } else {
+    /* Otherwise, we should:
+        1. Show all the articles except the template */
+    }
+    $('#category-filter').val('');
+  });
+};
+
+articleView.handleCategoryFilter = function() {
+  /* TODO: Just like we do for #author-filter above, we should also handle
+  change events on the #category-filter element. Be sure to reset the
+  #author-filter while you're at it! */
+};
+
+articleView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function() {
+    /* TODO:
+      1. Hide all of the .tab-content sections
+      2. Fade in the single .tab-content section that is
+        associated with the .tab element's data-content attribute.
+    */
+  });
+  $('.main-nav .tab:first').click();
+};
+
+articleView.setTeasers = function() {
+  // Truncate logic to show only first two elements within the article body.
+  $('.article-body *:nth-of-type(n+2)').hide();
+  /* TODO: Add a delegated event handler to reveal the remaining paragraphs.
+    When a .read-on link is clicked, we can:
+    1. Prevent the default action of a link.
+    2. Reveal everything in that particular article now.
+    3. Hide that read-on link!
+
+    // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
+  */
+};
+
+// TODO: Invoke all of the above functions (I mean, methods!):
